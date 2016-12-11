79b8e4b540a56e368206540330e389436b6dfffe
Bartłomiej Wójcik <bartlomiejwojcik92@gmail.com>
2016-12-11 11:14:33 +0100

    gulpfile created

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,17 @@
+'use strict';
+
 var gulp = require('gulp');
 var jshint = require('gulp-jshint');
 var browserify = require('browserify');
 var source = require('vinyl-source-stream');
 var sass = require('gulp-sass');
+var livereload = require('gulp-livereload');
 
 // configuration
 var paths = {
   allScripts: [
     'gulpfile.js',
-    'js/main.bundle.js',
-    'css/styles.css'
+    'js/modules/*.js',
   ]
 };
 
@@ -25,18 +27,23 @@ gulp.task('browserify', function() {
   return browserify('./js/main.js')
     .bundle()
     .pipe(source('main.bundle.js'))
-    .pipe(gulp.dest('./js/'));
+    .pipe(gulp.dest('./js/'))
+    .pipe(livereload());
 });
 
 // build styles.css from sass
 gulp.task('styles', function() {
   gulp.src('sass/styles.scss')
     .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('./css/'));
+    .pipe(gulp.dest('./css/'))
+    .pipe(livereload());
 });
 
 // watch files
 gulp.task('watch', function() {
+  livereload.listen();
   gulp.watch('js/**/*.js', ['browserify']);
   gulp.watch('sass/**/*.scss', ['styles']);
 });
+
+gulp.task('default', ['browserify','styles','watch']);
